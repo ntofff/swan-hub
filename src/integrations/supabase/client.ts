584d3a62e79c6a31114ec92e1db9d735bc6d1fd3
ntofff <nick.hb@hotmail.com>
2026-04-14 23:02:57 +0200

    Update client.ts

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -2,7 +2,7 @@ import { createClient } from '@supabase/supabase-js';
 import type { Database } from './types';
 
 const SUPABASE_URL = "https://oqiybworucfowcwllufg.supabase.co";
-const SUPABASE_PUBLISHABLE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9xaXlid29ydWNmb3djd2xsdWZnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzYxODk5OTEsImV4cCI6MjA5MTc2NTk5MX0.DvKpdnUWUzry4WFBkKp7FIm8-zfEs7Rf3IglYxgG4C4";
+const SUPABASE_PUBLISHABLE_KEY = "COLLE-TA-ANON-KEY-ICI";
 
 export const supabase = createClient<Database>(SUPABASE_URL, SUPABASE_PUBLISHABLE_KEY, {
   auth: {
@@ -10,5 +10,4 @@ export const supabase = createClient<Database>(SUPABASE_URL, SUPABASE_PUBLISHABL
     persistSession: true,
     autoRefreshToken: true,
   }
-});
 });
