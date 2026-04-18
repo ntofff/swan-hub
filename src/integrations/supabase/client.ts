606e59c116e93e576088240539b4b3535b4ebfa8
ntofff <nick.hb@hotmail.com>
2026-04-18 20:44:16 +0200

    Update client.ts
    desgign V2

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -6,7 +6,7 @@ import { createClient } from '@supabase/supabase-js';
 import type { Database } from './types';
 
 const SUPABASE_URL = 'https://oqiybworucfowcwllufg.supabase.co';
-const SUPABASE_PUBLISHABLE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9xaXlid29ydWNmb3djd2xsdWZnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA4NDUxNjYsImV4cCI6MjA3NjQyMTE2Nn0.ku_ur1Oqd1VAZKJxV_rLCH2BH-zD8UzZXw-gBPs0APA';
+const SUPABASE_PUBLISHABLE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9xaXlid29ydWNmb3djd2xsdWZnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzYxODk5OTEsImV4cCI6MjA5MTc2NTk5MX0.DvKpdnUWUzry4WFBkKp7FIm8-zfEs7Rf3IglYxgG4C4';
 
 export const supabase = createClient<Database>(
   SUPABASE_URL,
